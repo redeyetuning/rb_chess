145fb1db06ad00d01bffd898ab2f75aebeb819b2
Samuel Masters <redeyetuning@gmail.com>
2018-04-04 15:30:10 +0400

    Add Board #alpnum_coord_trnslt and #coord_alphnum_trnslt helpers.

--- lib/board.rb
+++ lib/board.rb
@@ -26,6 +26,12 @@ class Board
 		@board_state[[5,8]] = 'Queen.new("B")'
 	end
 
-	
+	def alphnum_coord_trnslt (alph_num_coord)
+		[alph_num_coord[0].ord-96, alph_num_coord[1].to_i]
+	end
+
+	def coord_alphnum_trnslt (coord)
+		(coord[0]+96).chr + coord[1].to_s
+	end
 
 end
