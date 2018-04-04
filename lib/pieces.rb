e755c8efd6c984071aa46feaa1c79770ba688f0a
Samuel Masters <redeyetuning@gmail.com>
2018-04-04 14:07:58 +0400

    Add pieces.rb with Class outlines for all pieces

---
+++ lib/pieces.rb
@@ -0,0 +1,49 @@
+class Pieces
+	def initialize
+	end
+
+	def mv_on_board?(strt, fin)
+	end
+end
+
+class Pawn < Pieces
+
+	def valid_move?(strt, fin)
+	end
+	
+end
+
+class Rook < Pieces
+
+	def valid_move?(strt, fin)
+	end
+	
+end
+
+class Knight < Pieces
+
+	def valid_move?(strt, fin)
+	end
+	
+end
+
+class Bishop < Pieces
+
+	def valid_move?(strt, fin)
+	end
+	
+end
+
+class King < Pieces
+
+	def valid_move?(strt, fin)
+	end
+	
+end
+
+class Queen < Pieces
+
+	def valid_move?(strt, fin)
+	end
+	
+end
