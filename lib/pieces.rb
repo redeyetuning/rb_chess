9f31bd9eed8de102af469e5b3eb777b1b8faa4b9
Samuel Masters <redeyetuning@gmail.com>
2018-04-04 15:55:40 +0400

    Add #valid_move? to King & Queen classes.

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -3,6 +3,7 @@ class Pieces
 	end
 
 	def mv_on_board?(strt, fin)
+		true if strt[0] + fin[0] <= 8 or strt[1] + fin[1] <= 8 
 	end
 end
 
@@ -37,13 +38,19 @@ end
 class King < Pieces
 
 	def valid_move?(strt, fin)
+		true if ( (strt[0]-strt[1]).abs == 0 and (fin[0]-fin[1]).abs == 1 ) or 
+						( (strt[0]-strt[1]).abs == 1 and (fin[0]-fin[1]).abs == 0 ) 
 	end
-	
 end
 
 class Queen < Pieces
 
 	def valid_move?(strt, fin)
+		true if ( (strt[0]-fin[0]).abs <= 8 and (strt[1]-strt[1]).abs == 0 ) or 
+						( (strt[1]-fin[1]).abs <= 8 and (strt[0]-strt[0]).abs == 0 ) or
+						( (strt[0]-fin[0]).abs == (strt[1]-fin[1]).abs ))
+	end 
+
 	end
 	
 end
