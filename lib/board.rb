76050bbf604cc73d226993d130e7a6f22a5a581b
Samuel Masters <redeyetuning@gmail.com>
2018-04-04 15:28:44 +0400

    Modify Board#populate_board to take numerical coordinates\

--- lib/board.rb
+++ lib/board.rb
@@ -8,22 +8,24 @@ class Board
 	end
 
 	def populate_board
-		tiles = [*1..8].repeated_permutation(2).to_a.map{|x| "#{(x[0]+96).chr}#{x[1]}"} #Array from "a1" -> "h8"
+		tiles = [*1..8].repeated_permutation(2).to_a #Array from "1,1" -> "8,8"
 		@board_state = Hash[tiles.map{|tile| [tile, nil]}]
 
-		["a2","b2","c2","d2","e2","f2","g2","h2"].each {|tile| @board_state[tile] = 'Pawn.new("W")'}
-		["a1","h1"].each {|tile| @board_state[tile] = 'Rook.new("W")'}
-		["b1","g1"].each {|tile| @board_state[tile] = 'Knight.new("W")'}
-		["c1","f1"].each {|tile| @board_state[tile] = 'Bishop.new("W")'}
-		@board_state["e1"] = 'King.new("W")'
-		@board_state["d1"] = 'Queen.new("W")'
+		[[1,2],[2,2],[3,2],[4,2],[5,2],[6,2],[7,2],[8,2]].each {|tile| @board_state[tile] = 'Pawn.new("W")'}
+		[[1,1],[8,1]].each {|tile| @board_state[tile] = 'Rook.new("W")'}
+		[[2,1],[7,1]].each {|tile| @board_state[tile] = 'Knight.new("W")'}
+		[[3,1],[6,1]].each {|tile| @board_state[tile] = 'Bishop.new("W")'}
+		@board_state[[5,1]] = 'King.new("W")'
+		@board_state[[4,1]] = 'Queen.new("W")'
 
-		["a7","b7","c7","d7","e7","f7","g7","h7"].each {|tile| @board_state[tile] = 'Pawn.new("B")'}
-		["a8","h8"].each {|tile| @board_state[tile] = 'Rook.new("B")'}
-		["b8","g8"].each {|tile| @board_state[tile] = 'Knight.new("B")'}
-		["c8","f8"].each {|tile| @board_state[tile] = 'Bishop.new("B")'}
-		@board_state["d8"] = 'King.new("B")'
-		@board_state["e8"] = 'Queen.new("B")'
+		[[1,7],[2,7],[3,7],[4,7],[5,7],[6,7],[7,7],[8,7]].each {|tile| @board_state[tile] = 'Pawn.new("B")'}
+		[[1,8],[8,8]].each {|tile| @board_state[tile] = 'Rook.new("B")'}
+		[[2,8],[7,8]].each {|tile| @board_state[tile] = 'Knight.new("B")'}
+		[[3,8],[6,8]].each {|tile| @board_state[tile] = 'Bishop.new("B")'}
+		@board_state[[4,8]] = 'King.new("B")'
+		@board_state[[5,8]] = 'Queen.new("B")'
 	end
+
+	
 
 end
