a18b86887d9fdb3b40603bbfd770d733062b1951
Samuel Masters <redeyetuning@gmail.com>
2018-04-08 14:33:35 +0400

    Update Bishop#valid_move? to #valid_moves which stores all possible moves in @moves

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -19,7 +19,6 @@ end
 class Rook < Pieces
 	
 	def valid_moves
-	
 		@moves =[]
 		
 		i = @cur_pos[0] + 1
@@ -44,12 +43,9 @@ class Rook < Pieces
 			@moves << [@cur_pos[0],j]
 			j -= 1
 		end
-
 	end 
 	
 end
-
-
 
 class Knight < Pieces
 
@@ -58,27 +54,27 @@ class Knight < Pieces
 		combos = [[1,2], [1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,1],[-2,-1]]
 		combos.each do |combo|
 			new_pos = [@cur_pos[0]+combo[0],@cur_pos[1]+combo[1]]
-			@moves << new_pos if $game.board_state[new_pos].nil? and new_pos[0]<9 and new_pos[1]<9  
+			@moves << new_pos if $game.board_state[new_pos].nil? and new_pos[0]<9 and new_pos[1]<9 and new_pos[0]>0 and new_pos[1]>0  
 		end
-
 	end 
-
-	def valid_move?(strt, fin)
-		if 	( (strt[0]-fin[0]).abs == 2 and (strt[1]-fin[1]).abs == 1 ) or 
-				( (strt[1]-fin[1]).abs == 2 and (strt[0]-fin[0]).abs == 1 )
-			return true
-		else return false
-		end
-	end
 	
 end
 
 class Bishop < Pieces
 
-	def valid_move?(strt, fin)
-		if ( (strt[0]-fin[0]).abs == (strt[1]-fin[1]).abs )
-			return true
-		else return false
+	def valid_moves 
+		@moves = []
+		combos = [[1,1],[1,-1],[-1,1],[-1,-1]]
+		combos.each do |combo|
+			puts i = @cur_pos[0] + combo[0]	
+			puts j = @cur_pos[1] + combo[1]
+			
+			while $game.board_state[[i,j]].nil? and i<9 and j<9 and i>0 and j>0
+				@moves << [i,j]
+				i += combo[0]
+				j += combo[1]
+			end
+
 		end 
 	end
 	
