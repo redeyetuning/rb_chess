20303d9fd98d4b14f0ca08bc36c7819d6b892a2a
Samuel Masters <redeyetuning@gmail.com>
2018-04-08 12:45:11 +0400

    Update Rook#valid_move? to #valid_moves which stores all possible moves in @moves

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,5 +1,10 @@
 class Pieces
-	def initialize
+	attr_accessor :cur_pos, :colour, :moves
+
+	def initialize cur_pos, colour
+		@cur_pos = cur_pos
+		@colour = colour
+		#valid_moves()
 	end
 
 	def mv_on_board?(strt, fin)
@@ -16,15 +21,41 @@ end
 
 class Rook < Pieces
 
-	def valid_move?(strt, fin)
-		if 	( (strt[0]-fin[0]).abs <= 8 and (strt[1]-fin[1]).abs == 0 ) or 
-				( (strt[1]-fin[1]).abs <= 8 and (strt[0]-fin[0]).abs == 0 )
-			return true
-		else return false
-		end 
+	
+	def valid_moves
+	
+		@moves =[]
+		
+		i = @cur_pos[0] + 1
+		while $game.board_state[[i,@cur_pos[1]]].nil? and i<9
+			@moves << [i,@cur_pos[1]]
+			i += 1
+		end
+	
+		i = @cur_pos[0] - 1
+		while $game.board_state[[i,@cur_pos[1]]].nil? and i>0
+			@moves << [i,@cur_pos[1]]
+			i -= 1
+		end
+	
+		j = @cur_pos[1] + 1
+		while $game.board_state[[@cur_pos[0],j]].nil? and j<9
+			@moves << [@cur_pos[0],j]
+			j += 1
+		end
+		j = @cur_pos[1] - 1
+		while $game.board_state[[@cur_pos[0],j] ].nil? and j>0
+			@moves << [@cur_pos[0],j]
+			j -= 1
+		end
+
 	end 
 	
+
+
 end
+
+
 
 class Knight < Pieces
 
